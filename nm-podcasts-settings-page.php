471dcde34e83a5829c483f6c2e53ae3cc16514cb
Shawn Beelman <shawn@sbgraphicdesign.com>
2015-07-20 15:15:41 -0500

    added google analytics property ID field
    used for download tracking

--- nm-podcasts-settings-page.php
+++ nm-podcasts-settings-page.php
@@ -1,6 +1,5 @@
 <?php
-class NMPodcastsSettingsPage
-{
+class NMPodcastsSettingsPage {
 	/**
 	 * Holds the values to be used in the fields callbacks
 	 */
@@ -9,8 +8,7 @@ class NMPodcastsSettingsPage
 	/**
 	 * Start up
 	 */
-	public function __construct()
-	{
+	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
 		add_action( 'admin_init', array( $this, 'page_init' ) );
 	}
@@ -18,8 +16,7 @@ class NMPodcastsSettingsPage
 	/**
 	 * Add options page
 	 */
-	public function add_plugin_page()
-	{
+	public function add_plugin_page() {
 		// submenu under custom post type menu
 		add_submenu_page(
 			'edit.php?post_type=nm_podcasts',
@@ -44,8 +41,7 @@ class NMPodcastsSettingsPage
 	/**
 	 * Options page callback
 	 */
-	public function create_admin_page()
-	{
+	public function create_admin_page() {
 		// Set class property
 		$this->options = get_option( 'nm_podcasts_options' );
 
@@ -74,8 +70,7 @@ class NMPodcastsSettingsPage
 	/**
 	 * Register and add settings
 	 */
-	public function page_init()
-	{		
+	public function page_init() {		
 		
 		register_setting(
 			'nm_podcasts_option_group', // Option group
@@ -147,6 +142,14 @@ class NMPodcastsSettingsPage
 			'nm-podcasts-admin', 
 			'section_one'
 		);
+
+		add_settings_field(
+			'google_analytics_tracking', 
+			'Google Analytics Account', 
+			array( $this, 'google_analytics_tracking_callback' ), 
+			'nm-podcasts-admin', 
+			'section_one'
+		);
 		
 		// END Fields	
 	}
@@ -156,8 +159,7 @@ class NMPodcastsSettingsPage
 	 *
 	 * @param array $input Contains all settings fields as array keys
 	 */
-	public function sanitize( $input )
-	{
+	public function sanitize( $input ) {
 		$new_input = array();
 		if( isset( $input['podcast_title'] ) )
 			$new_input['podcast_title'] = sanitize_text_field( $input['podcast_title'] );
@@ -180,67 +182,68 @@ class NMPodcastsSettingsPage
 		if( isset( $input['podcast_subcategory'] ) )
 			$new_input['podcast_subcategory'] = sanitize_text_field( $input['podcast_subcategory'] );
 
+		if( isset( $input['google_analytics_tracking'] ) )
+			$new_input['google_analytics_tracking'] = sanitize_text_field( $input['google_analytics_tracking'] );
+
 		return $new_input;
 	}
 
 	/** 
 	 * Print the Section text
 	 */
-	public function print_section_info()
-	{
+	public function print_section_info() {
 		print '<p>These settings are used for the podcast RSS feed.<br>Category options are listed <a href="http://www.apple.com/itunes/podcasts/specs.html#categories" target="_blank">here.</a></p>';
 	}
 
 	/** 
 	 * Get the settings option array and print one of its values
 	 */
-	public function podcast_title_callback()
-	{
+	public function podcast_title_callback() {
 		printf(
 			'<input type="text" id="podcast_title" name="nm_podcasts_options[podcast_title]" value="%s" />',
 			isset( $this->options['podcast_title'] ) ? esc_attr( $this->options['podcast_title']) : ''
 		);
 	}
-	public function itunes_subscribe_url_callback()
-	{
+	public function itunes_subscribe_url_callback() {
 		printf(
 			'<input type="text" id="itunes_subscribe_url" name="nm_podcasts_options[itunes_subscribe_url]" value="%s" style="width: 100%%;" />',
 			isset( $this->options['itunes_subscribe_url'] ) ? esc_attr( $this->options['itunes_subscribe_url']) : ''
 		);
 	}
-	public function stitcher_subscribe_url_callback()
-	{
+	public function stitcher_subscribe_url_callback() {
 		printf(
 			'<input type="text" id="stitcher_subscribe_url" name="nm_podcasts_options[stitcher_subscribe_url]" value="%s" style="width: 100%%;" />',
 			isset( $this->options['stitcher_subscribe_url'] ) ? esc_attr( $this->options['stitcher_subscribe_url']) : ''
 		);
 	}
-	public function cover_art_url_callback()
-	{
+	public function cover_art_url_callback() {
 		printf(
 			'<input type="text" id="cover_art_url" name="nm_podcasts_options[cover_art_url]" value="%s" style="width: 100%%;" /><br><em>Direct link to podcast cover art. Should be 1400x1400.</em>',
 			isset( $this->options['cover_art_url'] ) ? esc_attr( $this->options['cover_art_url']) : ''
 		);
 	}
-	public function podcast_summary_callback()
-	{
+	public function podcast_summary_callback() {
 		printf(
 			'<textarea type="textarea" id="podcast_summary" name="nm_podcasts_options[podcast_summary]" style="width: 100%%; height: 5em;">%s</textarea><br><em>Short description of the podcast.</em>',
 			isset( $this->options['podcast_summary'] ) ? esc_attr( $this->options['podcast_summary']) : ''
 		);
 	}
-	public function podcast_category_callback()
-	{
+	public function podcast_category_callback() {
 		printf(
 			'<input type="text" id="podcast_category" name="nm_podcasts_options[podcast_category]" value="%s" /><br><em>Top-level category. See list at link above.</em>',
 			isset( $this->options['podcast_category'] ) ? esc_attr( $this->options['podcast_category']) : ''
 		);
 	}
-	public function podcast_subcategory_callback()
-	{
+	public function podcast_subcategory_callback() {
 		printf(
 			'<input type="text" id="podcast_subcategory" name="nm_podcasts_options[podcast_subcategory]" value="%s" /><br><em>Secondary category. See list at link above.</em>',
 			isset( $this->options['podcast_subcategory'] ) ? esc_attr( $this->options['podcast_subcategory']) : ''
+		);
+	}
+	public function google_analytics_tracking_callback() {
+		printf(
+			'<input type="text" id="google_analytics_tracking" name="nm_podcasts_options[google_analytics_tracking]" value="%s" /><br><em>Google Analytics property ID for tracking downloads (e.g. "UA-000000-00").</em>',
+			isset( $this->options['google_analytics_tracking'] ) ? esc_attr( $this->options['google_analytics_tracking']) : ''
 		);
 	}
 }
