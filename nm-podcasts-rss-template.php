1d50328f81ab0823cee845b360e934d94ec44893
Shawn Beelman <shawn@sbgraphicdesign.com>
2015-07-20 15:15:12 -0500

    update file url for download tracking
    and multiple minor formatting edits

--- nm-podcasts-rss-template.php
+++ nm-podcasts-rss-template.php
@@ -11,26 +11,26 @@ $args = array( 'post_type' => 'nm_podcasts', 'posts_per_page' => 100 );
 $loop = new WP_Query( $args );
 
 // Output the XML header
-header('Content-Type: '.feed_content_type('rss-http').'; charset='.get_option('blog_charset'), true);
-echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
+header( 'Content-Type: '.feed_content_type( 'rss-http' ).'; charset='.get_option( 'blog_charset' ), true );
+echo '<?xml version="1.0" encoding="'.get_option( 'blog_charset' ).'"?'.'>';
 ?>
 <?php // Start the iTunes RSS Feed: https://www.apple.com/itunes/podcasts/specs.html ?>
 <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom">
 
   <channel>
-	<atom:link href="<?php echo get_bloginfo('url'); ?>/feed/podcast" rel="self" type="application/rss+xml" />
-    <title><?php echo !empty( $podcasts_options['podcast_title'] ) ? esc_attr( $podcasts_options['podcast_title'] ) : get_bloginfo('name') . ' Podcast'; ?></title>
-    <link><?php echo get_bloginfo('url'); ?></link>
+	<atom:link href="<?php echo get_bloginfo( 'url' ); ?>/feed/podcast" rel="self" type="application/rss+xml" />
+    <title><?php echo !empty( $podcasts_options['podcast_title'] ) ? esc_attr( $podcasts_options['podcast_title'] ) : get_bloginfo( 'name' ) . ' Podcast'; ?></title>
+    <link><?php echo get_bloginfo( 'url' ); ?></link>
     <language><?php echo get_bloginfo ( 'language' ); ?></language>
-    <copyright><?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?></copyright>
+    <copyright><?php echo date('Y'); ?> <?php echo get_bloginfo( 'name' ); ?></copyright>
     
-    <itunes:author><?php echo esc_attr( get_bloginfo('name') ); ?></itunes:author>
+    <itunes:author><?php echo esc_attr( get_bloginfo( 'name' ) ); ?></itunes:author>
     <itunes:summary><?php echo esc_attr( $podcasts_options['podcast_summary'] ); ?></itunes:summary>
-    <description><?php echo get_bloginfo('url'); ?></description>
+    <description><?php echo get_bloginfo( 'url' ); ?></description>
     
     <itunes:owner>
-      <itunes:name><?php echo esc_attr( get_bloginfo('name') ); ?></itunes:name>
-      <itunes:email><?php echo get_bloginfo('admin_email'); ?></itunes:email>
+      <itunes:name><?php echo esc_attr( get_bloginfo( 'name' ) ); ?></itunes:name>
+      <itunes:email><?php echo get_bloginfo( 'admin_email' ); ?></itunes:email>
     </itunes:owner>
     
     <itunes:image href="<?php echo esc_url( $podcasts_options['cover_art_url'] ); ?>" />
@@ -43,10 +43,10 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
     while ( $loop->have_posts() ) : $loop->the_post(); $pid = get_the_id(); ?>
     <item>
       <title><?php echo html_entity_decode( get_the_title() ); ?></title>
-      <itunes:author><?php echo get_bloginfo('name'); ?></itunes:author>
+      <itunes:author><?php echo get_bloginfo( 'name' ); ?></itunes:author>
       <itunes:summary><?php echo get_the_excerpt(); ?></itunes:summary>
       
-      <?php $file_url = get_post_meta( $pid, 'nm_podcast_file_url', true ); ?>
+      <?php $file_url = get_bloginfo( 'url' ) . '/download/?url=' . get_post_meta( $pid, 'nm_podcast_file_url', true ); ?>
       <enclosure url="<?php echo esc_url( $file_url ); ?>" length="<?php echo get_post_meta( $pid, 'nm_podcast_file_size', true ); ?>" type="audio/mpeg" />
       <guid><?php echo $file_url; ?></guid>
       <pubDate><?php the_time( 'D, d M Y H:i:s T') ?></pubDate>
